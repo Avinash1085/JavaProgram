7b9f7006327120d09b3390383460df2be09ac81b
Avinash1085 <84628476+Avinash1085@users.noreply.github.com>
2021-06-28 11:38:58 +0530

    Update Calculator.java

--- MyJavaProgms/Calculator.java
+++ MyJavaProgms/Calculator.java
@@ -5,7 +5,7 @@ public class Calculator
 	{
 		int num1,num2;
 		num1=45;
-		num2=21;
+		num2=23;
 		
 		System.out.println("# Assignment No: 3.");
 		System.out.println("Program Name : "+ "Simple Calculator.");
